afe53343c3a880233dd90e9abcd162143dd09936
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-04-27 15:03:48 +0530

    Add files via upload
    Spring profiles

---
+++ udemy-di-using-qualifiers/src/main/java/guru/springframework/udemy/UdemyDiApplication.java
@@ -0,0 +1,36 @@
+package guru.springframework.udemy;
+
+import guru.springframework.udemy.controllers.ConstructorInjectedController;
+import guru.springframework.udemy.controllers.MyController;
+import guru.springframework.udemy.controllers.PropertyInjectedController;
+import guru.springframework.udemy.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class UdemyDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(UdemyDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor" );
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
---
+++ udemy-di-using-qualifiers/src/test/java/guru/springframework/udemy/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.udemy.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.udemy.services.ConstructorGreetingService;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+
+    }
+}
