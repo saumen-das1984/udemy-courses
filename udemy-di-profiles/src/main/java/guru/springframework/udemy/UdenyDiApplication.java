23874ffcb43ec63026f3d23918764c5947f4e3f9
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-04-27 14:58:55 +0530

    Add files via upload
    Spring Profiles in Dependency Injection

---
+++ udemy-di-profiles/src/main/java/guru/springframework/udemy/UdenyDiApplication.java
@@ -0,0 +1,40 @@
+package guru.springframework.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.udemy.controllers.ConstructorInjectedController;
+import guru.springframework.udemy.controllers.I18nController;
+import guru.springframework.udemy.controllers.MyController;
+import guru.springframework.udemy.controllers.PropertyInjectedController;
+import guru.springframework.udemy.controllers.SetterInjectedController;
+
+@SpringBootApplication
+public class UdenyDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(UdenyDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		System.out.println("------- Primary Bean");
+		System.out.println(myController.sayHello());
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor" );
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
---
+++ udemy-di-profiles/src/main/java/guru/springframework/udemy/services/PropertyInjectedGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.udemy.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/27/19.
+ */
+@Service
+public class PropertyInjectedGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - Property";
+    }
+}
